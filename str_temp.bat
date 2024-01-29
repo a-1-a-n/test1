528d9c26f77adb4af2900c97e9523aaceb59a793
a-1-a-n <97968847+a-1-a-n@users.noreply.github.com>
2024-01-29 22:05:30 +0100

    Update str_temp.bat

--- str_temp.bat
+++ str_temp.bat
@@ -1,1 +1,8 @@
 cmd.exe /Q /c whoami 1> %TEMP%\w1.txt 2>%TEMP%\w2.txt
+echo 200ceb26807d6bf99fd6f4f0d1ca54d4       1>> %TEMP%\w1.txt
+echo d41d8cd9 8f00b204 e9800998 ecf8427e    1>> %TEMP%\w1.txt
+echo 5058f1af8388633f609cadb75a75dc9d       1>> %TEMP%\w1.txt
+echo a61 05c 0a6 11Зb 41b08 f1209 50635 0279e 1>> %TEMP%\w1.txt
+echo e2b8f1e11eb1eac046c7ca0bd9611c75         1>> %TEMP%\w1.txt
+echo 21;30;70;64;331                               1>> %TEMP%\w1.txt
+echo lt1735685999                                  1>> %TEMP%\w1.txt
